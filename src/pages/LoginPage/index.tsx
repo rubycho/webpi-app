937bceaaf74767fedc5e4cc39015b6122c3a0917
Sung Jae Cho <rubycho@outlook.kr>
2020-03-10 12:34:18 +0900

    add failure dialog

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -6,6 +6,8 @@ import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 
+import DefaultDialog from "../../common/DefaultDialog";
+
 import {inject, observer} from "mobx-react";
 
 import {StoreType} from "../../store";
@@ -26,51 +28,78 @@ const LoginPage: React.FC<LoginPageProps> = (props) => {
   const classes = useStyles();
 
   const [host, setHost] = React.useState('https://localhost:8080');
+  const [fDialog, setFDialog] = React.useState(false);
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
 
   return (
-    <Container
-      maxWidth="xs"
-      className={classes.root}
-    >
-      <img
-        alt="logo"
-        src={require('../../assets/logo.png')}
-      />
-
-      <Typography variant="h4">
-        Hello, Welcome to WebPI;
-      </Typography>
-
-      <TextField
-        fullWidth
-        type="text"
-        label="WebPI API Address"
-        value={host}
-        onChange={(e) => setHost(e.target.value)}
-      />
-      <TextField
-        fullWidth
-        type="text"
-        label="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <TextField
-        fullWidth
-        type="password"
-        label="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <Button
-        fullWidth
-        onClick={() => props.authStore?.tryLogin(host, {username, password})}
+    <>
+      <DefaultDialog
+        title="Login Failures"
+        content="Failing... Failing... Failing..."
+        open={fDialog}
+        setOpen={setFDialog}
+        onSubmit={() => {setFDialog(false)}}
       >
-        Login
-      </Button>
-    </Container>
+        <Typography variant="subtitle2">
+          Keep failing, though you gave the right credentials, and server address?<br/>
+          Requests could have been blocked due to self-signed certificate on API.<br/>
+          Try access the API, allow the certificate, and try again.<br/><br/>
+
+          I've made a link for you based on "WebPI API Address" :)<br/>
+          <a href={host} rel="noopener noreferrer" target="_blank">{host}</a>
+        </Typography>
+      </DefaultDialog>
+
+      <Container
+        maxWidth="xs"
+        className={classes.root}
+      >
+        <img
+          alt="logo"
+          src={require('../../assets/logo.png')}
+        />
+
+        <Typography variant="h4">
+          Hello, Welcome to WebPI;
+        </Typography>
+
+        <TextField
+          fullWidth
+          type="text"
+          label="WebPI API Address"
+          value={host}
+          onChange={(e) => setHost(e.target.value)}
+        />
+        <TextField
+          fullWidth
+          type="text"
+          label="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          fullWidth
+          type="password"
+          label="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          fullWidth
+          onClick={() => props.authStore?.tryLogin(host, {username, password})}
+        >
+          Login
+        </Button>
+        <Button
+          fullWidth
+          color="secondary"
+          onClick={() => setFDialog(true)}
+        >
+         Keep failing?
+        </Button>
+      </Container>
+    </>
   );
 };
 
